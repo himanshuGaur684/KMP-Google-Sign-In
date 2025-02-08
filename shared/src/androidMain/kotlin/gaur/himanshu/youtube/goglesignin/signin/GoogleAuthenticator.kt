c5f08f7e4c3431beb2e81ee15574a3929bbf5a74
Himanshu <himanshugaur684@gmail.com>
2025-02-08 19:10:28 +0530

    Final project for google sign in KMP Project ( Included all pods too )

--- shared/src/androidMain/kotlin/gaur/himanshu/youtube/goglesignin/signin/GoogleAuthenticator.kt
+++ shared/src/androidMain/kotlin/gaur/himanshu/youtube/goglesignin/signin/GoogleAuthenticator.kt
@@ -17,7 +17,7 @@ class GoogleAuthenticator(
     private val credentialManager: CredentialManager
 ) {
 
-    suspend fun login() : String{
+    suspend fun login(): String {
         try {
             val googleIdOption = GetGoogleIdOption.Builder()
                 .setServerClientId("458352816759-ls7libfra52161m07hrf4r1b5bn7pt54.apps.googleusercontent.com")
@@ -28,32 +28,29 @@ class GoogleAuthenticator(
                 .addCredentialOption(googleIdOption)
                 .build()
 
-            val result = credentialManager.getCredential(context,request)
+            val result = credentialManager.getCredential(context, request)
             val credential = result.credential
-            if(credential is CustomCredential && credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL){
+            if (credential is CustomCredential && credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
                 val googleIdTokenCredential = GoogleIdTokenCredential.createFrom(credential.data)
                 val idToken = googleIdTokenCredential.idToken
-                val authCredential = GoogleAuthProvider.getCredential(idToken,null)
+                val authCredential = GoogleAuthProvider.getCredential(idToken, null)
                 val fireBase = FirebaseAuth.getInstance()
-                val user=fireBase.signInWithCredential(authCredential).await().user
+                val user = fireBase.signInWithCredential(authCredential).await().user
                 return user?.displayName.toString()
             }
             return "SOmething went wrong"
-        }
-        catch (e:NoCredentialException){
+        } catch (e: NoCredentialException) {
             return "No email found in your device"
-        }
-        catch (e:GetCredentialException){
+        } catch (e: GetCredentialException) {
             return "GetCredentialException"
-        }
-        catch (e:Exception){
+        } catch (e: Exception) {
             return e.message.toString()
         }
 
 
     }
 
-    fun logout(){
+    fun logout() {
         FirebaseAuth.getInstance().signOut()
     }
 
--- shared/src/iosMain/kotlin/gaur/himanshu/youtube/goglesignin/signin/Platform.ios.kt
+++ shared/src/iosMain/kotlin/gaur/himanshu/youtube/goglesignin/signin/Platform.ios.kt
@@ -2,8 +2,9 @@ package gaur.himanshu.youtube.goglesignin.signin
 
 import platform.UIKit.UIDevice
 
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+class IOSPlatform : Platform {
+    override val name: String =
+        UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
